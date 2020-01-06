bf42aa31e360382373fbe6c912da3774760febed
ring0hacking <r1ng0hacking@gmail.com>
2020-01-06 11:38:08 +0200

    add vmx check

--- my_vt.c
+++ my_vt.c
@@ -1,10 +1,20 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <asm/processor.h>
+#include <asm/virtext.h>
 
 static int __init my_vt_init(void)
 {
 	printk("my_vt_init func\n");
+
+	if(cpu_has_vmx()){
+		printk("vmx support\n");
+	}else{
+		printk("vmx not support\n");
+		return 1;
+	}
+
 	return 0;
 }
 
